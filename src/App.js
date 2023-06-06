051829c4993669272066dd2884eeb336386763d6
Being-Murshid <murshid71704@gmail.com>
2023-06-06 06:52:50 -0700

    adding new line

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <TodoApp/>
       <div>
         <h1>this is a new line </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi aperiam illum accusamus dolore numquam facilis culpa libero! Numquam tempora, quam magnam omnis assumenda ea ipsum eum, ullam culpa facere in.</p>
       </div>
     </div>
 
