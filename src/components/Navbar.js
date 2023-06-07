70e941f76051c5be649c4c4de10319e2fc19366c
MuhammadEssa514 <muhammadessa15121472@gmail.com>
2023-06-06 20:08:58 -0700

    three

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,11 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-        <h1>i work on master branch </h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem minus magni sed praesentium libero odio! Excepturi vitae, exercitationem quasi autem voluptatem ipsam labore totam, eveniet omnis similique alias commodi.</p>
-        <button>Submit</button>
-
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>contact</li>
+      </ul>
     </div>
   )
 }
