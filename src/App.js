4d2e615f2a564e985a1e47efdc58f7c0802a5b20
Being-Murshid <murshid71704@gmail.com>
2023-06-08 06:10:18 -0700

    working on fetching data using useEffect function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import PureImpure from './components/PureImpure';
+// import Navbar from './components/Navbar';
+// import PureImpure from './components/PureImpure';
+import ReactHook from './components/ReactHook';
 // import Pr from './Pr';
 // import Form from './components/Form';
-import Saqi from './components/Saqi';
-import TodoApp from './components/TodoApp';
+// import Saqi from './components/Saqi';
+// import TodoApp from './components/TodoApp';
 
 function App() {
   
@@ -12,21 +13,11 @@ function App() {
     <div>
       {/* <Form placeholder="enter some thing here"/> */}
       {/* <Pr/> */}
-      <Saqi />
-      <TodoApp />
-      <div>
-        <h2>this is not in essa pc </h2>
-        <h1>this is a new line </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi aperiam
-          illum accusamus dolore numquam facilis culpa libero! Numquam tempora,
-          quam magnam omnis assumenda ea ipsum eum, ullam culpa facere in.
-        </p>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quisquam aperiam unde! Pariatur hic explicabo, soluta velit cum maiores ipsum voluptate quia voluptatibus repellendus, labore itaque nihil facere quae saepe?</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem similique nemo quae mollitia, cum accusamus illum at, voluptas recusandae molestiae fugit quam voluptate alias suscipit labore tempore ipsum consequuntur totam!</p>
-      </div>
-      <Navbar />
-      <PureImpure/>
+      {/* <Saqi /> */}
+      {/* <TodoApp /> */}
+      {/* <Navbar /> */}
+      {/* <PureImpure/> */}
+      <ReactHook/>
     </div>
 
     );
