e2f3cd231b8156e47046f19734c514052a2d31a3
Being-Murshid <murshid71704@gmail.com>
2023-06-12 06:44:08 -0700

    fix the error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import UseReducer from './components/UseReducer';
+// import './App.css';
+// import UseReducer from './components/UseReducer';
 import { useState } from "react";
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 // import PureImpure from './components/PureImpure';
-import ReactHook from './components/ReactHook';
-import CustomHook from './components/CustomHook';
-import Asif from './components/asif';
+// import ReactHook from './components/ReactHook';
+// import CustomHook from './components/CustomHook';
 import Form from "./components/Form";
 import Header from "./components/Header";
 import Task from "./components/Task";
@@ -75,12 +74,11 @@ function App() {
       {/* <Pr/> */}
       {/* <Saqi /> */}
       {/* <TodoApp /> */}
-      <Navbar link1='Home' link2='About' link3='Blogs' />
+      {/* <Navbar link1='Home' link2='About' link3='Blogs' /> */}
       {/* <PureImpure/> */}
-      <Asif/>
-      <ReactHook/>
+      {/* <ReactHook/>
       <UseReducer/>
-      <CustomHook/>
+      <CustomHook/> */}
       <Header title="Task Tracker" onAdd={handleAdd} showForm={showForm} />
       {showForm && <Form addFun={addFun} />}
       {tasks.length > 0 ? (
