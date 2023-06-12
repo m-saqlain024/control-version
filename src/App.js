dff0f88755c7743139abfaa5df335509c80cb2c2
Muhammad Saqlain <104670161+Being-Murshid@users.noreply.github.com>
2023-06-12 06:49:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import UseReducer from './components/UseReducer';
+
+// import UseReducer from './components/UseReducer';
 import { useState } from "react";
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 // import PureImpure from './components/PureImpure';
-import ReactHook from './components/ReactHook';
-import CustomHook from './components/CustomHook';
-import Asif from './components/asif';
+// import ReactHook from './components/ReactHook';
+// import CustomHook from './components/CustomHook';
+// import Asif from './components/asif';
 import Form from "./components/Form";
 import Header from "./components/Header";
 import Task from "./components/Task";
@@ -75,12 +75,12 @@ function App() {
       {/* <Pr/> */}
       {/* <Saqi /> */}
       {/* <TodoApp /> */}
-      <Navbar link1='Home' link2='About' link3='Blogs' />
+//       <Navbar link1='Home' link2='About' link3='Blogs' />
       {/* <PureImpure/> */}
-      <Asif/>
-      <ReactHook/>
-      <UseReducer/>
-      <CustomHook/>
+//       <Asif/>
+//       <ReactHook/>
+//       <UseReducer/>
+//       <CustomHook/>
       <Header title="Task Tracker" onAdd={handleAdd} showForm={showForm} />
       {showForm && <Form addFun={addFun} />}
       {tasks.length > 0 ? (
